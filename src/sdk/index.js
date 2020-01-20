46eadf4f68541a75abd671bb4384c95626d1901f
johnnyopao <jandresopao@gmail.com>
2020-01-20 15:24:50 -0500

    Expose validate methods

--- src/sdk/index.js
+++ src/sdk/index.js
@@ -1,9 +1,11 @@
 import linkReplace from './linkReplace';
 import oembed from './oembed';
 import textReplace from './textReplace';
+import validate from './validate';
 
 export {
   oembed,
   linkReplace,
-  textReplace
+  textReplace,
+  validate
 };
