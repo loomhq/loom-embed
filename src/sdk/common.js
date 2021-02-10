9f48927f8ed3421ff1224171542abd9ec174e057
Noah Lindner <noah.lindner@loom.com>
2021-02-10 12:45:27 -0800

    Modify base URL regex to accept staging and development URLs

--- src/sdk/common.js
+++ src/sdk/common.js
@@ -1,4 +1,5 @@
-const LOOM_BASE_REGEX_STR = '(https?:\/\/)?(www\.)?((use)?loom\.com\/share\/[a-f0-9]+)';
+// Regex url for staging, production, and local development
+const LOOM_BASE_REGEX_STR = '(https?:\/\/)?(www\.|stage\.)?((use)?(loom|loomlocal)\.com(:4444)?\/share\/[a-f0-9]+)';
 
 export const LOOM_URL_REGEX = new RegExp(`^${LOOM_BASE_REGEX_STR}$`);
 export const LOOM_URL_MATCH_REGEX = new RegExp(`${LOOM_BASE_REGEX_STR}`, 'g');
