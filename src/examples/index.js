d96bf4e2a2a8bd5374a422d3bfc5c5fb2583c2a2
Mitchell Fragala <ms.fragala@gmail.com>
2021-04-08 11:20:35 -0400

    Add Segment tracking snippet

--- src/examples/index.js
+++ src/examples/index.js
@@ -3,3 +3,14 @@ import ReactDOM from 'react-dom';
 import App from './components/App.js';
 
 ReactDOM.render(<App />, document.getElementById('loom-docs'));
+
+if (process.env.NODE_ENV === 'production') {
+  const script = document.createElement('script');
+  script.text = `
+    !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="pAxHiDtNYRUYzIvHwxDYit97K3ZX8p3d";analytics.SNIPPET_VERSION="4.13.2";
+    analytics.load("pAxHiDtNYRUYzIvHwxDYit97K3ZX8p3d");
+    analytics.page();
+    }}();
+  `;
+  document.body.append(script);
+}
